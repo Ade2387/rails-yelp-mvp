92141ad1384c2bd1197f38b1109f46654d3e37a7
Arno Debelle <debelle.a@gmail.com>
2022-02-16 13:08:01 +0100

    saving

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'restaurants', to: 'restaurants#index'
-  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
-  post 'restaurants', to: 'restaurants#create'
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-  # get 'restaurants/new'
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[show new create]
+  end
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
 end
